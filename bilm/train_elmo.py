5b80b9e9f1295c669d30eb7d9ddaaf0775f29ba2
Andrey Kutuzov <akutuzov72@gmail.com>
2021-02-15 16:12:29 +0100

    Code formatting

--- bilm/train_elmo.py
+++ bilm/train_elmo.py
@@ -16,59 +16,64 @@ def main(args):
     n_train_tokens = args.size
 
     options = {
-        'bidirectional': True,
-
-        'char_cnn': {'activation': 'relu',
-                     'embedding': {'dim': 16},
-                     'filters': [[1, 32],
-                                 [2, 32],
-                                 [3, 64],
-                                 [4, 128],
-                                 [5, 256],
-                                 [6, 512],
-                                 [7, 1024]],
-                     'max_characters_per_token': args.maxchar,
-                     'n_characters': 261,
-                     'n_highway': 2},
-
-        'dropout': 0.1,
-
-        'lstm': {
-            'cell_clip': 3,
-            'dim': 2048,
-            'n_layers': 2,
-            'proj_clip': 3,
-            'projection_dim': 512,
-            'use_skip_connections': True},
-
-        'all_clip_norm_val': 10.0,
-
-        'n_epochs': args.epochs,
-        'n_train_tokens': n_train_tokens,
-        'batch_size': batch_size,
-        'n_tokens_vocab': vocab.size,
-        'unroll_steps': 20,
-        'n_negative_samples_batch': 4096,
+        "bidirectional": True,
+        "char_cnn": {
+            "activation": "relu",
+            "embedding": {"dim": 16},
+            "filters": [
+                [1, 32],
+                [2, 32],
+                [3, 64],
+                [4, 128],
+                [5, 256],
+                [6, 512],
+                [7, 1024],
+            ],
+            "max_characters_per_token": args.maxchar,
+            "n_characters": 261,
+            "n_highway": 2,
+        },
+        "dropout": 0.1,
+        "lstm": {
+            "cell_clip": 3,
+            "dim": 2048,
+            "n_layers": 2,
+            "proj_clip": 3,
+            "projection_dim": 512,
+            "use_skip_connections": True,
+        },
+        "all_clip_norm_val": 10.0,
+        "n_epochs": args.epochs,
+        "n_train_tokens": n_train_tokens,
+        "batch_size": batch_size,
+        "n_tokens_vocab": vocab.size,
+        "unroll_steps": 20,
+        "n_negative_samples_batch": 4096,
     }
 
     prefix = args.train_prefix
-    data = BidirectionalLMDataset(prefix, vocab, test=False,
-                                  shuffle_on_load=True)
+    data = BidirectionalLMDataset(prefix, vocab, test=False, shuffle_on_load=True)
 
     tf_save_dir = args.save_dir
     tf_log_dir = args.save_dir
     train(options, data, n_gpus, tf_save_dir, tf_log_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--save_dir', help='Location of checkpoint files')
-    parser.add_argument('--vocab_file', help='Vocabulary file')
-    parser.add_argument('--train_prefix', help='Prefix for train files')
-    parser.add_argument('--size', type=int, help='Number of training tokens')
-    parser.add_argument('--epochs', type=int, default=3, help='Number of training epochs')
-    parser.add_argument('--maxchar', type=int, default=50, help='Maximum characters per token '
-                                                                '(longer tokens will be cropped)')
+    parser.add_argument("--save_dir", help="Location of checkpoint files")
+    parser.add_argument("--vocab_file", help="Vocabulary file")
+    parser.add_argument("--train_prefix", help="Prefix for train files")
+    parser.add_argument("--size", type=int, help="Number of training tokens")
+    parser.add_argument(
+        "--epochs", type=int, default=3, help="Number of training epochs"
+    )
+    parser.add_argument(
+        "--maxchar",
+        type=int,
+        default=50,
+        help="Maximum characters per token (longer tokens will be cropped)",
+    )
 
     arguments = parser.parse_args()
     main(arguments)
